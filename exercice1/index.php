f70648e727dbbae20503f809ce714aa101571c2a
Achille Kouassi <101454995+achillekouassi@users.noreply.github.com>
2023-02-20 16:32:13 +0100

    Update index.php

--- exercice1/index.php
+++ exercice1/index.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
+<?php include ('header.php'); ?>
 
 <h2>Bienvenue sur mon profil </h2>
 
-<?php
-include ('header.php');
+<?php include ('footer.php');?>
 
-include ('footer.php');
-?>
-</body>
-</html>
